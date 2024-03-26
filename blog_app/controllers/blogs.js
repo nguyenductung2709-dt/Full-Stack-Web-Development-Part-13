9bb8dee9e179c5029926102e2ce19927f74fc587
Duc Tung Nguyen <tungdtnguyen2709@gmail.com>
2024-03-27 00:31:44 +0200

    13.6

--- blog_app/controllers/blogs.js
+++ blog_app/controllers/blogs.js
@@ -3,31 +3,43 @@ const router = require('express').Router();
 const { Blog } = require('../models/index');
 
 router.get('/', async(req,res) => {
-    const blogs = await Blog.findAll()
-    console.log(blogs)
-    res.json(blogs)
+  const blogs = await Blog.findAll()
+  console.log(blogs)
+  res.json(blogs)
 })
 
 router.post('/', async(req,res) => {
-    try {
-        const newBlog = await Blog.create(req.body)
-        return res.json(newBlog)
-    }
-    catch (err) {
-        return res.status(400).json({ err })
-    }
+  try {
+    const newBlog = await Blog.create(req.body)
+    return res.json(newBlog)
+  }
+  catch (err) {
+    return res.status(400).json({ err })
+  }
 })
 
 router.delete('/:id', async(req,res) => {
-    const blog = await Blog.findByPk(req.params.id)
-    if (blog) {
-      Blog.destroy({
-        id: req.params.id
-      })
-      return res.json(blog)
-    } else {
+  const blog = await Blog.findByPk(req.params.id)
+  if (blog) {
+    Blog.destroy({
+      id: req.params.id
+    })
+    return res.json(blog)
+  } else {
       res.status(404).end()
-    }
+  }
+})
+
+router.put('/:id', async(req,res) => {
+  const blog = await Blog.findByPk(req.params.id)
+  if (blog) {
+    blog.likes = blog.likes + 1
+    await blog.save()
+    res.json(blog)
+  }
+  else {
+    res.status(404).end()
+  }
 })
 
 module.exports = router
