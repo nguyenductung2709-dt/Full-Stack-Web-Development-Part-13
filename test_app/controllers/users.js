41425b890887477824f5552890cedb897459b150
Duc Tung Nguyen <tungdtnguyen2709@gmail.com>
2024-03-27 23:04:24 +0200

    Learning before 13.17

--- test_app/controllers/users.js
+++ test_app/controllers/users.js
@@ -30,4 +30,42 @@ router.get('/:id', async (req, res) => {
   }
 })
 
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    } catch{
+      return res.status(401).json({ error: 'token invalid' })
+    }
+  }  else {
+    return res.status(401).json({ error: 'token missing' })
+  }
+  next()
+}
+
+const isAdmin = async (req, res, next) => {
+  const user = await User.findByPk(req.decodedToken.id)
+  if (!user.admin) {
+    return res.status(401).json({ error: 'operation not allowed' })
+  }
+  next()
+}
+
+router.put('/:username', tokenExtractor, isAdmin, async (req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: req.params.username
+    }
+  })
+
+  if (user) {
+    user.disabled = req.body.disabled
+    await user.save()
+    res.json(user)
+  } else {
+    res.status(404).end()
+  }
+})
+
 module.exports = router
--- test_app/models/user.js
+++ test_app/models/user.js
@@ -19,6 +19,15 @@ User.init({
     type: DataTypes.STRING,
     allowNull: false
   },
+
+  admin: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  },
+  disabled: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  },
 }, {
   sequelize,
   underscored: true,
