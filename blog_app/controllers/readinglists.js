80fdbf80bc5b1186c39757ad84160cb11c9980f2
Duc Tung Nguyen <tungdtnguyen2709@gmail.com>
2024-03-28 15:30:46 +0200

    13.22

--- blog_app/controllers/readinglists.js
+++ blog_app/controllers/readinglists.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const middleware = require('../util/middleware');
-const { ReadingList } = require('../models/readinglist');
+const { ReadingList, User } = require('../models/index');
 
 router.post('/', async(req, res) => {
     try {
@@ -10,6 +10,26 @@ router.post('/', async(req, res) => {
         next(err);
     }
 })
+
+router.put('/:id', middleware.tokenExtractor, async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const user = await User.findByPk(req.decodedToken.id);
+        if (!user) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+        const changedList = await ReadingList.findByPk(id);
+        console.log(user.id)
+        if (!changedList || changedList.userId !== user.id) {
+            return res.status(404).json({ error: 'Reading list not found or unauthorized' });
+        }
+        changedList.readingState = true;
+        await changedList.save();
+        res.json(changedList);
+    } catch (err) {
+        next(err); 
+    }
+});
 
 router.use(middleware.errorHandler);
 
